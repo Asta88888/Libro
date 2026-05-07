b15db3b533e4f402ee598c8e52594ef719fdc509
Anastasiya Soloviova <mafanya.as@mail.ru>
2026-05-07 19:12:13 +0300

    add permissions and pagination for books

--- books/views.py
+++ books/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Book, Genre, Author
 from .serializers import BookSerializer, GenreSerializer, AuthorSerializer
+from .pagination import StandardPagination
+from .permissions import IsAdminOrAuthenticatedReadOnly
 
 class BookViewSet(ModelViewSet):
     """
@@ -9,6 +11,11 @@ class BookViewSet(ModelViewSet):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class = StandardPagination
+    permission_classes = [IsAdminOrAuthenticatedReadOnly]
+    filterset_fields = ("author", "genres")
+    search_fields = ("title", "description",)
+    ordering_fields = ("title", "created_at", "publish_date",)
 
 class GenreViewSet(ModelViewSet):
     """
@@ -17,6 +24,8 @@ class GenreViewSet(ModelViewSet):
     """
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    pagination_class = StandardPagination
+    permission_classes = [IsAdminOrAuthenticatedReadOnly]
 
 class AuthorViewSet(ModelViewSet):
     """
@@ -25,4 +34,6 @@ class AuthorViewSet(ModelViewSet):
     """
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    pagination_class = StandardPagination
+    permission_classes = [IsAdminOrAuthenticatedReadOnly]
 
