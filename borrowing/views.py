023822355d7f6938eb7de76df6de3653395d7ccf
Anastasiya Soloviova <mafanya.as@mail.ru>
2026-05-08 03:26:05 +0300

    add views, urls for users

--- borrowing/views.py
+++ borrowing/views.py
@@ -3,17 +3,35 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 
+
 from .models import Borrowing
 from .serializers import BorrowingSerializer
 from .services import borrow_book, return_book, BorrowingError
 from inventory.models import BookCopy
+from .permissions import IsAdminOrOwner
+from books.pagination import StandardPagination
 
 class BorrowingViewSet(ModelViewSet):
     """
     API для выдачи и возврата книг.
     """
-    queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = [IsAdminOrOwner]
+    pagination_class = StandardPagination
+
+    filterset_fields = ("status", "user")
+    search_fields = ("book_copy__book__title", "user__email")
+    ordering_fields = ("borrowed_at", "due_date", "status")
+
+    def get_queryset(self):
+        """
+        Администратор видит все выдачи.
+        Пользователь — только свои.
+        """
+        user = self.request.user
+        if user.is_staff:
+            return Borrowing.objects.all()
+        return Borrowing.objects.filter(user=user)
 
     @action(detail=False, methods=["post"])
     def borrow(self, request):
@@ -24,10 +42,17 @@ class BorrowingViewSet(ModelViewSet):
             book_copy_id = request.data.get("book_copy_id")
             days = int(request.data.get("days", 14))
 
+            if not book_copy_id:
+                return Response(
+                    {"error": "book_copy_id обязателен"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
             book_copy = BookCopy.objects.get(id=book_copy_id)
 
             borrowing = borrow_book(user=request.user, book_copy=book_copy, days=days)
             return Response(BorrowingSerializer(borrowing).data, status=status.HTTP_201_CREATED)
+
         except BookCopy.DoesNotExist:
             return Response(
                 {"error": "Книга не найдена"},
@@ -46,9 +71,10 @@ class BorrowingViewSet(ModelViewSet):
         """
         try:
             borrowing = self.get_object()
-            borrowing = return_book(borrowing)
+            result = return_book(borrowing)
 
-            return Response(BorrowingSerializer(borrowing).data, status=status.HTTP_200_OK)
+
+            return Response(BorrowingSerializer(result).data, status=status.HTTP_200_OK)
         except BorrowingError as e:
             return Response(
                 {"error": str(e)},
--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import User
+from .serializers import UserSerializer
+from .permissions import IsAdmin, IsSelfOrAdmin
 
-# Create your views here.
+class UserViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [AllowAny()]
+        if self.action in ["list", "destroy"]:
+            return [IsAdmin()]
+        return [IsAuthenticated()]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_authenticated and user.role == "admin":
+            return User.objects.all()
+
+        return User.objects.filter(id=user.id)
