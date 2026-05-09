567c254d0f87b7fc8f51f451ceed547f8d6e7cc7
Anastasiya Soloviova <mafanya.as@mail.ru>
2026-05-09 17:29:31 +0300

    add urls, views, serializer to libraries

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,22 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("books.urls")),
+
+    path("api/users/", include("users.urls")),
+    path("api/books/", include("books.urls")),
+    path("api/libraries/", include("libraries.urls")),
+    path("api/inventory/", include("inventory.urls")),
+    path("api/borrowing/", include("borrowing.urls")),
+
+    path("swagger/", include("drf_yasg.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
---
+++ libraries/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+from .views import LibraryViewSet
+
+router = SimpleRouter()
+router.register(r"libraries", LibraryViewSet, basename="libraries")
+
+urlpatterns = [
+    path("", include(router.urls))
+]
